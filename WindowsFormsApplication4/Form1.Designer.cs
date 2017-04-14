451c7cb922a36b16a39d5c0fd8fcf164db9c7016
Janis <janis03050055@gmail.com>
2017-04-15 02:00:07 +0800

    C#

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBardistortion = new System.Windows.Forms.TrackBar();
+            this.trackBardistortionValue = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.adjustbutton = new System.Windows.Forms.Button();
+            this.savebutton = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.encode = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lossy_label = new System.Windows.Forms.Label();
+            this.decode = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBardistortion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "圖片輸入";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(541, 317);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "壓縮";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(19, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(295, 280);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(322, 31);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(295, 280);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(320, 322);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "選擇色彩數量";
+            // 
+            // trackBardistortion
+            // 
+            this.trackBardistortion.LargeChange = 8;
+            this.trackBardistortion.Location = new System.Drawing.Point(403, 317);
+            this.trackBardistortion.Maximum = 256;
+            this.trackBardistortion.Minimum = 2;
+            this.trackBardistortion.Name = "trackBardistortion";
+            this.trackBardistortion.Size = new System.Drawing.Size(104, 45);
+            this.trackBardistortion.SmallChange = 2;
+            this.trackBardistortion.TabIndex = 5;
+            this.trackBardistortion.Value = 2;
+            this.trackBardistortion.Scroll += new System.EventHandler(this.trackBardistortion_Scroll);
+            // 
+            // trackBardistortionValue
+            // 
+            this.trackBardistortionValue.AutoSize = true;
+            this.trackBardistortionValue.Location = new System.Drawing.Point(502, 322);
+            this.trackBardistortionValue.Name = "trackBardistortionValue";
+            this.trackBardistortionValue.Size = new System.Drawing.Size(0, 12);
+            this.trackBardistortionValue.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "原始圖片";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(322, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "壓縮圖片預覽";
+            // 
+            // adjustbutton
+            // 
+            this.adjustbutton.Enabled = false;
+            this.adjustbutton.Location = new System.Drawing.Point(541, 347);
+            this.adjustbutton.Name = "adjustbutton";
+            this.adjustbutton.Size = new System.Drawing.Size(75, 23);
+            this.adjustbutton.TabIndex = 9;
+            this.adjustbutton.Text = "調整";
+            this.adjustbutton.UseVisualStyleBackColor = true;
+            this.adjustbutton.Click += new System.EventHandler(this.adjustbutton_Click);
+            // 
+            // savebutton
+            // 
+            this.savebutton.Enabled = false;
+            this.savebutton.Location = new System.Drawing.Point(541, 377);
+            this.savebutton.Name = "savebutton";
+            this.savebutton.Size = new System.Drawing.Size(75, 23);
+            this.savebutton.TabIndex = 10;
+            this.savebutton.Text = "存檔";
+            this.savebutton.UseVisualStyleBackColor = true;
+            this.savebutton.Click += new System.EventHandler(this.savebutton_Click);
+            // 
+            // encode
+            // 
+            this.encode.Location = new System.Drawing.Point(778, 318);
+            this.encode.Name = "encode";
+            this.encode.Size = new System.Drawing.Size(75, 23);
+            this.encode.TabIndex = 11;
+            this.encode.Text = "編碼儲存";
+            this.encode.UseVisualStyleBackColor = true;
+            this.encode.Click += new System.EventHandler(this.encode_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 360);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "失真率：";
+            // 
+            // lossy_label
+            // 
+            this.lossy_label.AutoSize = true;
+            this.lossy_label.Location = new System.Drawing.Point(79, 360);
+            this.lossy_label.Name = "lossy_label";
+            this.lossy_label.Size = new System.Drawing.Size(41, 12);
+            this.lossy_label.TabIndex = 13;
+            this.lossy_label.Text = "？？？";
+            // 
+            // decode
+            // 
+            this.decode.Location = new System.Drawing.Point(859, 318);
+            this.decode.Name = "decode";
+            this.decode.Size = new System.Drawing.Size(75, 23);
+            this.decode.TabIndex = 14;
+            this.decode.Text = "解碼顯示";
+            this.decode.UseVisualStyleBackColor = true;
+            this.decode.Click += new System.EventHandler(this.decode_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(639, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "解碼圖片預覽";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(639, 32);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(295, 280);
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(126, 317);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "文字輸入";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 387);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "壓縮率：";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(79, 387);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 12);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "？？？";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 410);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.decode);
+            this.Controls.Add(this.lossy_label);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.encode);
+            this.Controls.Add(this.savebutton);
+            this.Controls.Add(this.adjustbutton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBardistortionValue);
+            this.Controls.Add(this.trackBardistortion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "圖片壓縮";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBardistortion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBardistortion;
+        private System.Windows.Forms.Label trackBardistortionValue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button adjustbutton;
+        private System.Windows.Forms.Button savebutton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button encode;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lossy_label;
+        private System.Windows.Forms.Button decode;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
